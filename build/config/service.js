11655190e607dd586b8719b86d6642884ecc7f45
saxibuni <1129130574@qq.com>
2017-01-17 10:12:02 +0800

    Update service.js

--- build/config/service.js
+++ build/config/service.js
@@ -1,5 +1,5 @@
 var config = {
-	"debug" : true,
-	"service": "ws://gs.keno.staging.spadegaming.biz:7788/websocket",
+	"debug" : true,	
+  "service": "wss://gs.keno.staging.spadegaming.biz:7788/websocket",
 	"trendsUrl": "http://192.168.0.61:8081/trends/"
 };
