b4d91e0a9d440b8e4678a6c957170b5959c642f5
saxibuni <1129130574@qq.com>
2017-01-17 18:24:24 +0800

    Update service.js

--- build/config/service.js
+++ build/config/service.js
@@ -1,5 +1,5 @@
 var config = {
 	"debug" : true,	
   "service": "ws://kenolobby.staging.inplaymatrix.com:8008/websocket",
-	"trendsUrl": "https://github.com/saxibuni/LK-mobile/trends/"
+	"trendsUrl": "../trends/"
 };
