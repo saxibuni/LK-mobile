86d26295324b9aa8baf51b24e08e705210735cbf
saxibuni <1129130574@qq.com>
2017-01-17 10:17:02 +0800

    Update service.js

--- build/config/service.js
+++ build/config/service.js
@@ -1,5 +1,5 @@
 var config = {
 	"debug" : true,	
-  "service": "wss://gs.keno.staging.spadegaming.biz:7788/websocket",
+  "service": "wss://kenolobby.staging.inplaymatrix.com:8008/websocket",
 	"trendsUrl": "http://192.168.0.61:8081/trends/"
 };
