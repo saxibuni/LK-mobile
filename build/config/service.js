12b3291c3d8dbb0adda5ae86225beb5ef069f37b
saxibuni <1129130574@qq.com>
2017-01-17 10:18:40 +0800

    Update service.js

--- build/config/service.js
+++ build/config/service.js
@@ -1,5 +1,5 @@
 var config = {
 	"debug" : true,	
-  "service": "wss://kenolobby.staging.inplaymatrix.com:8008/websocket",
+  "service": "ws://kenolobby.staging.inplaymatrix.com:8008/websocket",
 	"trendsUrl": "http://192.168.0.61:8081/trends/"
 };
